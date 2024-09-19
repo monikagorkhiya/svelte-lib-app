68b326599010f212755eb746ab4643a8963a4646
gorkhiyamonika <gorkhiyamonika@gmail.com>
2024-09-19 17:35:59 +0530

    store add

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,7 @@
 import Button from './Button.svelte';
 import Table from './Table.svelte';
 import DynamicComponent from './Dynamic.svelte';
+import DecrementButton from './DecrementButton.svelte';
+import IncrementButton from './IncrementButton.svelte';
 
-export { Button, Table, DynamicComponent };
+export { Button, Table, DynamicComponent,DecrementButton,IncrementButton };
