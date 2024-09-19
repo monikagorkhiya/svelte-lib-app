307f27d700aa6d771917bb0d620d08f535ef6fb4
gorkhiyamonika <gorkhiyamonika@gmail.com>
2024-09-19 14:07:01 +0530

    add svelte component button and table component as a library

---
+++ src/lib/Button.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let label = "Default Label"; // Default value for the label
+    export let classStyle = "Default Button Style"; // Default value for the label
+  </script>
+
+  <button class={classStyle}>{label}</button>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Button from '$lib/Button.svelte';
+	import TableComponent from '$lib/Table.svelte';
+	let label = 'Click Me!';
+	let classStyle = 'btn btn-custom';
+
+	let data = [
+		{ name: 'John Doe', email: 'john@email.com', phone: '+91846665646', designation: 'Engineer' },
+		{
+			name: 'Jane Smith',
+			email: 'jane@email.com',
+			phone: '+91886665656',
+			designation: 'Developer'
+		},
+		{ name: 'Sam Wilson', email: 'sam@email.com', phone: '+91816665656', designation: 'Manager' }
+	];
+</script>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<hr />
+<p>1. This is testing button which is render by library</p>
+
+<Button {classStyle} {label} />
+<hr />
+
+<p>2. This is testing Table which is render by library</p>
+<TableComponent tableData={data} />
+<hr />
